1ac78b136c2e8acb2672a4b686cc6c93a7d13ac0
David Esperalta <info@davidesperalta.com>
2016-01-24 14:19:37 +0100

    Code beauty is important!

--- angular-canvas-gauge.js
+++ angular-canvas-gauge.js
@@ -32,18 +32,18 @@ angular.module('angular-canvas-gauge', []).directive('canvasGauge', function() {
     link: function(scope, element, attributes) {
       var
         options = {
-		      renderTo: attributes.id,
+          renderTo: attributes.id,
           value: 0,
-		      width: 200,
-		      height: 200,
-		      title: false,
-		      maxValue: 100,
-		      minValue: 0,
-		      majorTicks: [],
-		      minorTicks: 10,
-		      strokeTicks: true,
-		      units: false,
-		      valueFormat: {
+          width: 200,
+          height: 200,
+          title: false,
+          maxValue: 100,
+          minValue: 0,
+          majorTicks: [],
+          minorTicks: 10,
+          strokeTicks: true,
+          units: false,
+          valueFormat: {
             'int': 3,
             'dec': 2
           },
@@ -51,20 +51,20 @@ angular.module('angular-canvas-gauge', []).directive('canvasGauge', function() {
             'int': 1,
             'dec': 0
           },
-		      glow: true,
-		      animation : {
-			      delay: 10,
-			      duration: 250,
-			      fn: 'cycle'
-		      },
-		      colors: {
-			      plate: '#fff',
-			      majorTicks: '#444',
-			      minorTicks: '#666',
-			      title: '#888',
-			      units: '#888',
-			      numbers: '#444',
-			      needle: {
+          glow: true,
+          animation: {
+            delay: 10,
+            duration: 250,
+            fn: 'cycle'
+          },
+          colors: {
+            plate: '#fff',
+            majorTicks: '#444',
+            minorTicks: '#666',
+            title: '#888',
+            units: '#888',
+            numbers: '#444',
+            needle: {
               start: 'rgba(240, 128, 128, 1)',
               end: 'rgba(255, 160, 122, .9)',
               circle: {
@@ -95,7 +95,7 @@ angular.module('angular-canvas-gauge', []).directive('canvasGauge', function() {
               innerStart: '#fafafa',
               innerEnd: '#ccc'
             }
-		      },
+          },
           circles: {
             outerVisible: true,
             middleVisible: true,
@@ -107,20 +107,20 @@ angular.module('angular-canvas-gauge', []).directive('canvasGauge', function() {
           valueText: {
             visible: true
           },
-		      highlights: [{
-			      from: 20,
-			      to: 60,
-			      color: '#eee'
-		      }, {
-			      from: 60,
-			      to: 80,
-			      color: '#ccc'
-		      }, {
-			      from: 80,
-			      to: 100,
-			      color: '#999'
-		      }]
-	      };
+          highlights: [{
+            from: 20,
+            to: 60,
+            color: '#eee'
+          }, {
+            from: 60,
+            to: 80,
+            color: '#ccc'
+          }, {
+            from: 80,
+            to: 100,
+            color: '#999'
+          }]
+        };
 
       var
         gauge = new Gauge(options);
@@ -135,168 +135,190 @@ angular.module('angular-canvas-gauge', []).directive('canvasGauge', function() {
             return result;
           }
 
-			    var
+          var
             hls = value.match(/(?:(?:-?\d*\.)?(-?\d+){1,2} ){2}(?:(?:#|0x)?(?:[0-9A-F|a-f]){3,8}|rgba?\(.*?\))/g);
 
           if (hls === null) {
             return result;
           }
 
-				  for (var j = 0, l = hls.length; j < l; j++) {
-				    var
-					    cfg = hls[j].trim().split( /\s+/),
-						  hlCfg = {};
+          for (var j = 0, l = hls.length; j < l; j++) {
+            var
+              cfg = hls[j].trim().split(/\s+/),
+              hlCfg = {};
 
-					  if (cfg[0] && cfg[0] != '') {
-					    hlCfg.from = cfg[0];
-					  }
+            if (cfg[0] && cfg[0] != '') {
+              hlCfg.from = cfg[0];
+            }
 
-					  if (cfg[1] && cfg[1] != '') {
-					    hlCfg.to = cfg[1];
-					  }
+            if (cfg[1] && cfg[1] != '') {
+              hlCfg.to = cfg[1];
+            }
 
-					  if (cfg[2] && cfg[2] != '') {
-					    hlCfg.color = cfg[2];
-					  }
+            if (cfg[2] && cfg[2] != '') {
+              hlCfg.color = cfg[2];
+            }
 
-					  result.push(hlCfg);
-			    }
+            result.push(hlCfg);
+          }
           return result;
-      };
+        };
 
-      attributes.$observe('glow', function(value){
-        gauge.updateConfig({glow: value === 'true'});
+      attributes.$observe('glow', function(value) {
+        gauge.updateConfig({
+          glow: value === 'true'
+        });
       });
 
-      attributes.$observe('value', function(value){
+      attributes.$observe('value', function(value) {
         gauge.setValue(value);
       });
 
-      attributes.$observe('title', function(value){
-        gauge.updateConfig({title: value});
+      attributes.$observe('title', function(value) {
+        gauge.updateConfig({
+          title: value
+        });
       });
 
-      attributes.$observe('units', function(value){
-        gauge.updateConfig({units: value});
+      attributes.$observe('units', function(value) {
+        gauge.updateConfig({
+          units: value
+        });
       });
 
-      attributes.$observe('width', function(value){
-        gauge.updateConfig({width: value});
+      attributes.$observe('width', function(value) {
+        gauge.updateConfig({
+          width: value
+        });
       });
 
-      attributes.$observe('height', function(value){
-        gauge.updateConfig({height: value});
+      attributes.$observe('height', function(value) {
+        gauge.updateConfig({
+          height: value
+        });
       });
 
-      attributes.$observe('minValue', function(value){
-        gauge.updateConfig({minValue: value});
+      attributes.$observe('minValue', function(value) {
+        gauge.updateConfig({
+          minValue: value
+        });
       });
 
-      attributes.$observe('maxValue', function(value){
-        gauge.updateConfig({maxValue: parseFloat(value)});
+      attributes.$observe('maxValue', function(value) {
+        gauge.updateConfig({
+          maxValue: parseFloat(value)
+        });
       });
 
-      attributes.$observe('minorTicks', function(value){
-        gauge.updateConfig({minorTicks: parseFloat(value)});
+      attributes.$observe('minorTicks', function(value) {
+        gauge.updateConfig({
+          minorTicks: parseFloat(value)
+        });
       });
 
-      attributes.$observe('majorTicks', function(value){
-        gauge.updateConfig({majorTicks: value.split(' ')});
+      attributes.$observe('majorTicks', function(value) {
+        gauge.updateConfig({
+          majorTicks: value.split(' ')
+        });
       });
 
-      attributes.$observe('highlights', function(value){
-        gauge.updateConfig({highlights: parseHightlights(value)});
+      attributes.$observe('highlights', function(value) {
+        gauge.updateConfig({
+          highlights: parseHightlights(value)
+        });
       });
 
-      attributes.$observe('strokeTicks', function(value){
-        gauge.updateConfig({strokeTicks: value});
+      attributes.$observe('strokeTicks', function(value) {
+        gauge.updateConfig({
+          strokeTicks: value
+        });
       });
 
-      attributes.$observe('animationFn', function(value){
+      attributes.$observe('animationFn', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.animation.fn = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsPlate', function(value){
+      attributes.$observe('colorsPlate', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.plate = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsUnits', function(value){
+      attributes.$observe('colorsUnits', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.units = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsTitle', function(value){
+      attributes.$observe('colorsTitle', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.title = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsNeedleStart', function(value){
+      attributes.$observe('colorsNeedleStart', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.needle.start = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsNeedleEnd', function(value){
+      attributes.$observe('colorsNeedleEnd', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.needle.end = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsMinorTicks', function(value){
+      attributes.$observe('colorsMinorTicks', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.minorTicks = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('colorsMajorTicks', function(value){
+      attributes.$observe('colorsMajorTicks', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.colors.majorTicks = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('valueboxVisible', function(value){
+      attributes.$observe('valueboxVisible', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.valueBox.visible = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('valuetextVisible', function(value){
+      attributes.$observe('valuetextVisible', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.valueText.visible = value;
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('circleOuterVisible', function(value){
+      attributes.$observe('circleOuterVisible', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.circles.outerVisible = value === 'true';
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('circleMiddleVisible', function(value){
+      attributes.$observe('circleMiddleVisible', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.circles.middleVisible = value === 'true';
         gauge.updateConfig(config);
       });
 
-      attributes.$observe('circleInnerVisible', function(value){
+      attributes.$observe('circleInnerVisible', function(value) {
         var
           config = gauge.updateConfig({}).config;
         config.circles.innerVisible = value === 'true';
